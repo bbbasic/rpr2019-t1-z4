df925b9713709e7c6fcaf61fbd8a85347b673cf6
bbbasic <ebasic3@etf.unsa.ba>
2020-11-02 00:06:46 +0100

    Dodane metode i atributi u klase Artikl Korpu i Supermarket

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,48 @@
 package ba.unsa.etf.rpr;
+//korpa.dodajArtikl(a)
+//korpa.getArtikli()
+//korpa.izbaciArtiklSaKodom(kod)
+//korpa.dajUkupnuCijenuArtikala()
 
 public class Korpa {
+    private Artikl[] korpa = new Artikl[50];
+    private int uneseno;
+
+    public Artikl[] getArtikli() {
+        return korpa;
+    }
+
+    public boolean dodajArtikl(Artikl artikl) {
+        if(uneseno < 50) {
+            korpa[uneseno] = new Artikl(artikl.getNaziv(), artikl.getCijena(), artikl.getKod());
+            uneseno++;
+            return true;
+        }
+        return false;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for(int i = 0; i < uneseno; i++)
+            suma = suma + korpa[i].getCijena();
+        return  suma;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < uneseno; i++) {
+            if(korpa[i].getKod().equals(kod)) {
+                //isti je
+                //posto vraca Artikl napravi ga i vrati ga
+                Artikl novi = new Artikl(korpa[i].getNaziv(),korpa[i].getCijena(), korpa[i].getKod());
+                //treba sve pomjeriti za jedno ulijevo
+                int j;
+                for(j = i; j < uneseno; j++)
+                    korpa[j] = korpa[j + 1];
+                korpa[j] = null;
+                uneseno--;
+                return  novi;
+            }
+        }
+        return  null;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] supermarket = new Artikl[100];
+    private int uneseno;
+
+    public Artikl[] getArtikli() {
+        return supermarket;
+    }
+
+    //supermarket.dodajArtikl(new Artikl("Biciklo", 1000, "1"));
+    public void dodajArtikl(Artikl artikl) {
+        supermarket[uneseno] = new Artikl(artikl.getNaziv(), artikl.getCijena(), artikl.getKod());
+        uneseno++;
+    }
+    //Artikl a = supermarket.izbaciArtiklSaKodom(kod);
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < uneseno; i++) {
+            if(supermarket[i].getKod().equals(kod)) {
+                //isti je
+                //posto vraca Artikl napravi ga i vrati ga
+                Artikl novi = new Artikl(supermarket[i].getNaziv(),supermarket[i].getCijena(), supermarket[i].getKod());
+                //treba sve pomjeriti za jedno ulijevo
+                int j;
+                for(j = i; j < uneseno; j++) {
+                    supermarket[j] = supermarket[j + 1];
+                }
+                supermarket[j] = null;
+                uneseno--;
+                return  novi;
+            }
+        }
+        return  null;
+    }
+
+
 }
